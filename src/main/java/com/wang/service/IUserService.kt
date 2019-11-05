1d33d38200c9ef3d6e048d87a70118efcf95d5ba
wangl <lwang18@mail.greenriver.edu>
2019-11-05 18:01:44 +0800

    Kotlin_spring_boot项目优化

--- src/main/java/com/wang/service/IUserService.kt
+++ src/main/java/com/wang/service/IUserService.kt
@@ -2,6 +2,24 @@ package com.debug.service
 
 import com.wang.entity.UserInfo
 
+/***
+ *
+*
+* 描    述：
+*
+* 创 建 者： @author wl
+* 创建时间： 2019/11/5 17:51
+* 创建描述：
+*
+* 修 改 者：
+* 修改时间：
+* 修改描述：
+*
+* 审 核 者：
+* 审核时间：
+* 审核描述：
+*
+ */
 interface IUserService {
 
     fun findUserById(userId: Int): UserInfo
